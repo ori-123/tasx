99217f179a905a2b30bfe97516d0506f15d2f29d
ori-123 <orirodatos@gmail.com>
2023-11-02 11:48:20 +0100

    create mock put route for task controller

--- src/main/java/com/codecool/tasx/controller/TaskController.java
+++ src/main/java/com/codecool/tasx/controller/TaskController.java
@@ -4,8 +4,10 @@ import com.codecool.tasx.controller.dto.company.CompanyResponsePrivateDTO;
 import com.codecool.tasx.controller.dto.company.CompanyResponsePublicDTO;
 import com.codecool.tasx.controller.dto.project.ProjectCreateRequestDto;
 import com.codecool.tasx.controller.dto.project.ProjectResponsePrivateDTO;
+import com.codecool.tasx.controller.dto.task.TaskCreateRequestDto;
 import com.codecool.tasx.controller.dto.task.TaskResponsePrivateDto;
 import com.codecool.tasx.controller.dto.task.TaskResponsePublicDto;
+import com.codecool.tasx.controller.dto.task.TaskUpdateRequestDto;
 import com.codecool.tasx.controller.dto.user.UserResponsePublicDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -69,7 +71,7 @@ public class TaskController {
     @PostMapping
     public ResponseEntity<?> createTask(
             @PathVariable long companyId, @PathVariable long projectId,
-            @RequestBody ProjectCreateRequestDto taskDetails) {
+            @RequestBody TaskCreateRequestDto taskDetails) {
         try {
             //TODO: impl
             TaskResponsePrivateDto taskResponseDetails = new TaskResponsePrivateDto(
@@ -90,6 +92,32 @@ public class TaskController {
         } catch (Exception e) {
             return ResponseEntity.status(500).body(
                     Map.of("error", "Failed to create task"));
+        }
+    }
+
+    @PutMapping("{/taskId}")
+    public ResponseEntity<?> updateTaskById(
+            @PathVariable long companyId, @PathVariable long projectId, @PathVariable long taskId,
+            @RequestBody TaskUpdateRequestDto taskDetails) {
+        try {
+            TaskResponsePrivateDto taskResponseDetails = new TaskResponsePrivateDto(
+                    taskDetails.description(), 4L, new ProjectResponsePrivateDTO(
+                    projectId, "Mock project name", "Mock project description",
+                    new CompanyResponsePrivateDTO(companyId, "Mock Company name", "Mock company description",
+                            new UserResponsePublicDto(1L, "Company Owner")),
+                    new UserResponsePublicDto(2L, "Project Owner")
+            ),
+                    new CompanyResponsePrivateDTO(companyId, "Mock Company name", "Mock company description",
+                            new UserResponsePublicDto(1L, "Company Owner")),
+                    new UserResponsePublicDto(6L, "Task Owner")
+            );
+
+            return ResponseEntity.status(HttpStatus.OK).body(Map.of(
+                    "message", "Task with ID" + projectId + " updated successfully",
+                    "data", taskResponseDetails));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(
+                    Map.of("error", "Failed to update task with ID " + taskId));
         }
     }
 }
