843d3b9396b2bd63f77f9e6e3989b61a26d7e36a
DNadas98 <125133206+DNadas98@users.noreply.github.com>
2023-11-02 10:58:28 +0100

    create user entity, service and required DTO

---
+++ src/main/java/com/codecool/tasx/model/user/User.java
@@ -0,0 +1,58 @@
+package com.codecool.tasx.model.user;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class User {
+
+  @Id
+  @SequenceGenerator(name = "user_id_sequence", sequenceName = "user_id_sequence")
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_sequence")
+  private Long id;
+
+  @Column(/*unique = true*/)
+  private String username;
+
+  public User() {
+
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    User user = (User) o;
+    return Objects.equals(id, user.id) && Objects.equals(username, user.username);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, username);
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+      "id=" + id +
+      ", username='" + username + '\'' +
+      '}';
+  }
+}
