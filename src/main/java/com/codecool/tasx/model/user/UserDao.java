6faea1c8b0bdae2690438647a5ef4559c95ba057
DNadas98 <125133206+DNadas98@users.noreply.github.com>
2023-11-02 21:51:21 +0100

    create a branch wiht only mock controllers for demo

--- src/main/java/com/codecool/tasx/model/user/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.tasx.model.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDao extends JpaRepository<User,Long> {
-}
