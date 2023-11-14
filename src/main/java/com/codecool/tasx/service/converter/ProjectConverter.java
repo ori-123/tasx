ba3f082035062a16d5b67df3b7c8cff6db11759a
oridev-123 <124574469+ori-123@users.noreply.github.com>
2023-11-15 00:14:35 +0100

    add method for converting to private dto to project converter

--- src/main/java/com/codecool/tasx/service/converter/ProjectConverter.java
+++ src/main/java/com/codecool/tasx/service/converter/ProjectConverter.java
@@ -1,5 +1,6 @@
 package com.codecool.tasx.service.converter;
 
+import com.codecool.tasx.controller.dto.project.ProjectResponsePrivateDTO;
 import com.codecool.tasx.controller.dto.project.ProjectResponsePublicDTO;
 import com.codecool.tasx.model.company.project.Project;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,20 @@ import java.util.stream.Collectors;
 
 @Service
 public class ProjectConverter {
+  private final UserConverter userConverter;
+  private final TaskConverter taskConverter;
 
   public ProjectResponsePublicDTO getProjectResponsePublicDto(Project project) {
     return new ProjectResponsePublicDTO(project.getCompany().getId(), project.getId(),
       project.getName(), project.getDescription());
+  }
+
+  public ProjectResponsePrivateDTO getProjectResponsePrivateDto(Project project) {
+    return new ProjectResponsePrivateDTO(project.getCompany().getId(), project.getId(), project.getName(),
+            project.getDescription(), project.getStartDate(), project.getDeadline(),
+            userConverter.getUserResponsePublicDto(project.getProjectOwner()),
+            userConverter.getUserResponsePublicDtos(project.getAssignedEmployees()),
+            taskConverter.getTaskResponsePublicDtos(project.getTasks()));
   }
 
   public List<ProjectResponsePublicDTO> getProjectResponsePublicDtos(List<Project> projects) {
