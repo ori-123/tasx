79b17cfb05a9bc025217f5f04981df091cc0c327
oridev-123 <124574469+ori-123@users.noreply.github.com>
2023-11-15 02:02:33 +0100

    add read logic to project controller

--- src/main/java/com/codecool/tasx/controller/ProjectController.java
+++ src/main/java/com/codecool/tasx/controller/ProjectController.java
@@ -7,6 +7,12 @@ import com.codecool.tasx.controller.dto.project.ProjectResponsePrivateDTO;
 import com.codecool.tasx.controller.dto.project.ProjectResponsePublicDTO;
 import com.codecool.tasx.controller.dto.project.ProjectUpdateRequestDto;
 import com.codecool.tasx.controller.dto.user.UserResponsePublicDto;
+import com.codecool.tasx.exception.project.ProjectNotFoundException;
+import com.codecool.tasx.service.populate.MockDataProvider;
+import com.codecool.tasx.service.project.ProjectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,110 +23,96 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/companies/{companyId}/projects")
 public class ProjectController {
-/*
-  @GetMapping
-  public ResponseEntity<?> getAllProjects(@PathVariable Long companyId) {
-    try {
-      //TODO: impl
-      List<ProjectResponsePublicDTO> projects = List.of(
-        new ProjectResponsePublicDTO(1L, "Mock Project 1",
-          "Public project details", new CompanyResponsePublicDTO(
-          companyId, "Mock Project 1", "Public project details")),
-        new ProjectResponsePublicDTO(2L, "Mock Project 2",
-          "Public project details", new CompanyResponsePublicDTO(
-          companyId, "Mock Project 1", "Public project details")));
+    private final ProjectService projectService;
+    private final MockDataProvider mockDataProvider;
+    private final Logger logger;
 
-      return ResponseEntity.status(HttpStatus.OK).body(Map.of("data", projects));
-    } catch (Exception e) {
-      //TODO: handle other exceptions
-      return ResponseEntity.status(500).body(
-        Map.of("error", "Failed to load projects"));
+    @Autowired
+    public ProjectController(ProjectService projectService, MockDataProvider mockDataProvider) {
+        this.projectService = projectService;
+        this.mockDataProvider = mockDataProvider;
+        logger = LoggerFactory.getLogger(this.getClass());
     }
-  }
 
-  @GetMapping("/{projectId}")
-  public ResponseEntity<?> getProjectById(
-    @PathVariable Long companyId,
-    @PathVariable Long projectId) {
-    try {
-      //TODO: impl
-      ProjectResponsePrivateDTO project = new ProjectResponsePrivateDTO(
-        projectId,
-        "Mock Project " + projectId,
-        "Project details to be seen only by the assigned employees",
-        new CompanyResponsePrivateDTO(companyId, "Mock company " + companyId,
-          "Company details to be seen only by the employees of the company",
-          new UserResponsePublicDto(1L, "Company Owner")),
-        new UserResponsePublicDto(2L, "Project Owner"));
-
-      return ResponseEntity.status(HttpStatus.OK).body(Map.of("data", project));
-    } catch (Exception e) {
-      //TODO: handle other exceptions
-      return ResponseEntity.status(500).body(
-        Map.of("error", "Failed to load details of project with ID " + projectId));
+    private Long getUserId() {
+        //TODO: get user from auth context
+        return mockDataProvider.getAllUsers().get(0).userId();
     }
-  }
 
-  @PostMapping
-  public ResponseEntity<?> createProject(
-    @PathVariable Long companyId,
-    @RequestBody ProjectCreateRequestDto projectDetails) {
-    try {
-      //TODO: impl
-      ProjectResponsePrivateDTO projectResponseDetails = new ProjectResponsePrivateDTO(
-        1L, projectDetails.name(), projectDetails.description(),
-        new CompanyResponsePrivateDTO(companyId, "Mock company " + companyId,
-          "Company details to be seen only by the employees of the company",
-          new UserResponsePublicDto(1L, "Company Owner")),
-        new UserResponsePublicDto(2L, "Project Owner"));
+      @GetMapping
+      public ResponseEntity<?> getAllProjects(@PathVariable Long companyId) {
+        List<ProjectResponsePublicDTO> projects = projectService.getAllProjects();
+          return ResponseEntity.status(HttpStatus.OK).body(Map.of("data", projects));
+      }
 
-      return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
-        "message", "Project created successfully",
-        "data", projectResponseDetails));
-    } catch (Exception e) {
-      //TODO: handle other exceptions
-      return ResponseEntity.status(500).body(
-        Map.of("error", "Failed to create project"));
-    }
-  }
+      @GetMapping("/{projectId}")
+      public ResponseEntity<?> getProjectById(
+        @PathVariable Long companyId,
+        @PathVariable Long projectId) {
+        Long userId = getUserId();
+        ProjectResponsePrivateDTO project = projectService.getProjectById(userId, projectId)
+                .orElseThrow(() -> new ProjectNotFoundException(projectId));
+          return ResponseEntity.status(HttpStatus.OK).body(Map.of("data", project));
+      }
 
-  @PutMapping("/{projectId}")
-  public ResponseEntity<?> updateProject(
-    @PathVariable Long companyId, @PathVariable Long projectId, @RequestBody
-  ProjectUpdateRequestDto projectDetails) {
-    try {
-      //TODO: impl
-      ProjectResponsePrivateDTO projectResponseDetails = new ProjectResponsePrivateDTO(
-        projectId, projectDetails.name(), projectDetails.description(),
-        new CompanyResponsePrivateDTO(companyId, "Mock company " + companyId,
-          "Company details to be seen only by the employees of the company",
-          new UserResponsePublicDto(1L, "Company Owner")),
-        new UserResponsePublicDto(2L, "Project Owner"));
+      @PostMapping
+      public ResponseEntity<?> createProject(
+        @PathVariable Long companyId,
+        @RequestBody ProjectCreateRequestDto projectDetails) {
+        try {
+          //TODO: impl
+          ProjectResponsePrivateDTO projectResponseDetails = new ProjectResponsePrivateDTO(
+            1L, projectDetails.name(), projectDetails.description(),
+            new CompanyResponsePrivateDTO(companyId, "Mock company " + companyId,
+              "Company details to be seen only by the employees of the company",
+              new UserResponsePublicDto(1L, "Company Owner")),
+            new UserResponsePublicDto(2L, "Project Owner"));
 
-      return ResponseEntity.status(HttpStatus.OK).body(Map.of(
-        "message", "Project with ID " + projectId + " updated successfully",
-        "data", projectResponseDetails));
-    } catch (Exception e) {
-      //TODO: handle other exceptions
-      return ResponseEntity.status(500).body(
-        Map.of("error", "Failed to update project with ID " + projectId));
-    }
-  }
+          return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
+            "message", "Project created successfully",
+            "data", projectResponseDetails));
+        } catch (Exception e) {
+          //TODO: handle other exceptions
+          return ResponseEntity.status(500).body(
+            Map.of("error", "Failed to create project"));
+        }
+      }
 
-  @DeleteMapping("/{projectId}")
-  public ResponseEntity<?> deleteProject(
-    @PathVariable Long companyId, @PathVariable Long projectId) {
-    try {
-      //TODO: impl
-      return ResponseEntity.status(HttpStatus.OK).body(Map.of(
-        "message",
-        "Project with ID " + projectId + " deleted successfully"));
-    } catch (Exception e) {
-      //TODO: handle other exceptions
-      return ResponseEntity.status(500).body(
-        Map.of("error", "Failed to delete project with ID " + projectId));
-    }
-  }
+      @PutMapping("/{projectId}")
+      public ResponseEntity<?> updateProject(
+        @PathVariable Long companyId, @PathVariable Long projectId, @RequestBody
+      ProjectUpdateRequestDto projectDetails) {
+        try {
+          //TODO: impl
+          ProjectResponsePrivateDTO projectResponseDetails = new ProjectResponsePrivateDTO(
+            projectId, projectDetails.name(), projectDetails.description(),
+            new CompanyResponsePrivateDTO(companyId, "Mock company " + companyId,
+              "Company details to be seen only by the employees of the company",
+              new UserResponsePublicDto(1L, "Company Owner")),
+            new UserResponsePublicDto(2L, "Project Owner"));
 
- */
+          return ResponseEntity.status(HttpStatus.OK).body(Map.of(
+            "message", "Project with ID " + projectId + " updated successfully",
+            "data", projectResponseDetails));
+        } catch (Exception e) {
+          //TODO: handle other exceptions
+          return ResponseEntity.status(500).body(
+            Map.of("error", "Failed to update project with ID " + projectId));
+        }
+      }
+
+      @DeleteMapping("/{projectId}")
+      public ResponseEntity<?> deleteProject(
+        @PathVariable Long companyId, @PathVariable Long projectId) {
+        try {
+          //TODO: impl
+          return ResponseEntity.status(HttpStatus.OK).body(Map.of(
+            "message",
+            "Project with ID " + projectId + " deleted successfully"));
+        } catch (Exception e) {
+          //TODO: handle other exceptions
+          return ResponseEntity.status(500).body(
+            Map.of("error", "Failed to delete project with ID " + projectId));
+        }
+      }
 }
