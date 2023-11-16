f9203c708802a8d1e812509fca786764227c1f24
DNadas98 <125133206+DNadas98@users.noreply.github.com>
2023-11-16 09:27:02 +0100

    update project controller

--- src/main/java/com/codecool/tasx/controller/ProjectController.java
+++ src/main/java/com/codecool/tasx/controller/ProjectController.java
@@ -7,13 +7,10 @@ import com.codecool.tasx.controller.dto.project.ProjectResponsePublicDTO;
 import com.codecool.tasx.controller.dto.project.ProjectUpdateRequestDto;
 import com.codecool.tasx.exception.company.CompanyNotFoundException;
 import com.codecool.tasx.exception.project.ProjectNotFoundException;
-import com.codecool.tasx.model.company.CompanyDao;
-import com.codecool.tasx.model.company.project.Project;
-import com.codecool.tasx.model.user.User;
-import com.codecool.tasx.model.user.UserDao;
 import com.codecool.tasx.service.company.CompanyService;
-import com.codecool.tasx.service.populate.MockDataProvider;
 import com.codecool.tasx.service.project.ProjectService;
+import com.codecool.tasx.service.security.AuthProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,77 +24,78 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/companies/{companyId}/projects")
 public class ProjectController {
-    private final CompanyService companyService;
-    private final ProjectService projectService;
-    private final MockDataProvider mockDataProvider;
-    private final Logger logger;
+  private final CompanyService companyService;
+  private final ProjectService projectService;
+  private final AuthProvider authProvider;
+  private final Logger logger;
 
-    @Autowired
-    public ProjectController(CompanyService companyService, ProjectService projectService, MockDataProvider mockDataProvider) {
-        this.companyService = companyService;
-        this.projectService = projectService;
-        this.mockDataProvider = mockDataProvider;
-        logger = LoggerFactory.getLogger(this.getClass());
-    }
+  @Autowired
+  public ProjectController(
+    CompanyService companyService, ProjectService projectService,
+    AuthProvider authProvider) {
+    this.companyService = companyService;
+    this.projectService = projectService;
+    this.authProvider = authProvider;
+    logger = LoggerFactory.getLogger(this.getClass());
+  }
 
-    private Long getUserId() {
-        //TODO: get user from auth context
-        return mockDataProvider.getAllUsers().get(0).userId();
-    }
+  @GetMapping
+  public ResponseEntity<?> getAllProjects(
+    @PathVariable Long companyId, HttpServletRequest request) {
+    Long userId = authProvider.getUserId(request);
+    CompanyResponsePrivateDTO company = companyService.getCompanyById(userId, companyId)
+      .orElseThrow(() -> new CompanyNotFoundException(companyId));
+    List<ProjectResponsePublicDTO> projects = projectService.getAllProjects(company.companyId());
+    return ResponseEntity.status(HttpStatus.OK).body(Map.of("data", projects));
+  }
 
-      @GetMapping
-      public ResponseEntity<?> getAllProjects(@PathVariable Long companyId) {
-        Long userId = getUserId();
-        CompanyResponsePrivateDTO company = companyService.getCompanyById(userId, companyId)
-                .orElseThrow(() -> new CompanyNotFoundException(companyId));
-        List<ProjectResponsePublicDTO> projects = projectService.getAllProjects(company.companyId());
-          return ResponseEntity.status(HttpStatus.OK).body(Map.of("data", projects));
-      }
+  @GetMapping("/{projectId}")
+  public ResponseEntity<?> getProjectById(
+    @PathVariable Long companyId, @PathVariable Long projectId, HttpServletRequest request) {
+    Long userId = authProvider.getUserId(request);
+    CompanyResponsePrivateDTO company = companyService.getCompanyById(userId, companyId)
+      .orElseThrow(() -> new CompanyNotFoundException(companyId));
+    ProjectResponsePrivateDTO project = projectService.getProjectById(
+        userId, projectId, company.companyId())
+      .orElseThrow(() -> new ProjectNotFoundException(projectId));
+    return ResponseEntity.status(HttpStatus.OK).body(Map.of("data", project));
+  }
 
-      @GetMapping("/{projectId}")
-      public ResponseEntity<?> getProjectById(@PathVariable Long companyId, @PathVariable Long projectId) {
-        Long userId = getUserId();
-          CompanyResponsePrivateDTO company = companyService.getCompanyById(userId, companyId)
-                  .orElseThrow(() -> new CompanyNotFoundException(companyId));
-        ProjectResponsePrivateDTO project = projectService.getProjectById(userId, projectId, company.companyId())
-                .orElseThrow(() -> new ProjectNotFoundException(projectId));
-          return ResponseEntity.status(HttpStatus.OK).body(Map.of("data", project));
-      }
+  @PostMapping
+  public ResponseEntity<?> createProject(
+    @PathVariable Long companyId,
+    @RequestBody ProjectCreateRequestDto projectDetails, HttpServletRequest request) {
+    Long userId = authProvider.getUserId(request);
 
-      @PostMapping
-      public ResponseEntity<?> createProject(
-        @PathVariable Long companyId,
-        @RequestBody ProjectCreateRequestDto projectDetails) {
-        Long userId = getUserId();
+    ProjectResponsePrivateDTO projectResponseDetails = projectService.createProject(
+      projectDetails, userId);
+    return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
+      "message", "Project created successfully",
+      "data", projectResponseDetails));
+  }
 
-        ProjectResponsePrivateDTO projectResponseDetails = projectService.createProject(projectDetails, userId);
-          return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
-                  "message", "Project created successfully",
-                  "data", projectResponseDetails));
-      }
+  @PutMapping("/{projectId}")
+  public ResponseEntity<?> updateProject(
+    @PathVariable Long companyId, @PathVariable Long projectId, @RequestBody
+  ProjectUpdateRequestDto projectDetails, HttpServletRequest request) {
+    Long userId = authProvider.getUserId(request);
 
-      @PutMapping("/{projectId}")
-      public ResponseEntity<?> updateProject(
-        @PathVariable Long companyId, @PathVariable Long projectId, @RequestBody
-      ProjectUpdateRequestDto projectDetails) {
-        Long userId = getUserId();
+    ProjectResponsePrivateDTO projectResponseDetails =
+      projectService.updateProject(projectDetails, userId, projectId);
 
-        ProjectResponsePrivateDTO projectResponseDetails =
-                projectService.updateProject(projectDetails, userId, projectId);
+    return ResponseEntity.status(HttpStatus.OK).body(Map.of(
+      "message", "Project with ID " + projectId + " updated successfully",
+      "data", projectResponseDetails));
+  }
 
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of(
-                  "message", "Project with ID " + projectId + " updated successfully",
-                  "data", projectResponseDetails));
-      }
+  @DeleteMapping("/{projectId}")
+  public ResponseEntity<?> deleteProject(
+    @PathVariable Long companyId, @PathVariable Long projectId, HttpServletRequest request) {
+    Long userId = authProvider.getUserId(request);
+    projectService.deleteProject(projectId, userId);
 
-      @DeleteMapping("/{projectId}")
-      public ResponseEntity<?> deleteProject(
-        @PathVariable Long companyId, @PathVariable Long projectId) {
-        Long userId = getUserId();
-        projectService.deleteProject(projectId, userId);
-
-        return ResponseEntity.status(HttpStatus.OK).body(Map.of(
-                "message",
-                "Project with ID " + projectId + " deleted successfully"));
-      }
+    return ResponseEntity.status(HttpStatus.OK).body(Map.of(
+      "message",
+      "Project with ID " + projectId + " deleted successfully"));
+  }
 }
