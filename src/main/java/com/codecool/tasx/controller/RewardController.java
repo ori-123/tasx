471848dd893712e6318dd32e79eac5fd81390bcf
ori-123 <orirodatos@gmail.com>
2023-11-28 12:14:25 +0100

    add create logic to reward controller

--- src/main/java/com/codecool/tasx/controller/RewardController.java
+++ src/main/java/com/codecool/tasx/controller/RewardController.java
@@ -1,15 +1,13 @@
 package com.codecool.tasx.controller;
 
+import com.codecool.tasx.controller.dto.reward.RewardCreateRequestDto;
 import com.codecool.tasx.controller.dto.reward.RewardResponseDto;
 import com.codecool.tasx.exception.reward.RewardNotFoundException;
 import com.codecool.tasx.service.company.reward.RewardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -37,5 +35,12 @@ public class RewardController {
         return ResponseEntity.status(HttpStatus.OK).body(Map.of("data", reward));
     }
 
+    @PostMapping
+    public ResponseEntity<?> createReward(@PathVariable Long companyId,
+                                          @RequestBody RewardCreateRequestDto rewardDetails) {
+        RewardResponseDto rewardResponseDetails = rewardService.createReward(rewardDetails, companyId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                Map.of("message", "Reward created successfully", "data", rewardResponseDetails));
+    }
 
 }
