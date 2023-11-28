17e305ffcd2a43a82a87bc3f1bbbd4b83542b6e1
oridev-123 <124574469+ori-123@users.noreply.github.com>
2023-11-28 01:06:02 +0100

    implement read logic in task controller

--- src/main/java/com/codecool/tasx/controller/TaskController.java
+++ src/main/java/com/codecool/tasx/controller/TaskController.java
@@ -18,5 +18,23 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/companies/{companyId}/projects/{projectId}/tasks")
 public class TaskController {
+    private final TaskService taskService;
 
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllTasks(@PathVariable Long companyId, @PathVariable Long projectId) {
+        List<TaskResponsePublicDto> tasks = taskService.getAllTasks(projectId, companyId);
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("data", tasks));
+    }
+
+    @GetMapping("/{taskId}")
+    public ResponseEntity<?> getTaskById(@PathVariable Long companyId, @PathVariable Long projectId, @PathVariable Long taskId) {
+        TaskResponsePublicDto task = taskService.getTaskById(taskId, projectId, companyId)
+                .orElseThrow(() -> new TaskNotFoundException(taskId));
+        return ResponseEntity.status(HttpStatus.OK).body(Map.of("data", task));
+    }
 }
