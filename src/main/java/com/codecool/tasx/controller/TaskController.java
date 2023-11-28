23f1f863788fa245620e35d9707035ae8abd1bf6
oridev-123 <124574469+ori-123@users.noreply.github.com>
2023-11-28 01:09:04 +0100

    remove some unnecessary imports

--- src/main/java/com/codecool/tasx/controller/TaskController.java
+++ src/main/java/com/codecool/tasx/controller/TaskController.java
@@ -3,8 +3,6 @@ package com.codecool.tasx.controller;
 import com.codecool.tasx.controller.dto.task.TaskCreateRequestDto;
 import com.codecool.tasx.controller.dto.task.TaskResponsePublicDto;
 import com.codecool.tasx.controller.dto.task.TaskUpdateRequestDto;
-import com.codecool.tasx.exception.company.CompanyNotFoundException;
-import com.codecool.tasx.exception.project.ProjectNotFoundException;
 import com.codecool.tasx.exception.task.TaskNotFoundException;
 import com.codecool.tasx.service.company.project.task.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
